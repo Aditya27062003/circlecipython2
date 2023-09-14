1dee0e428a0b4b543ec5018f7758ab0b8d91f0a8
aditya <adityasingh666.model@gmail.com>
2023-09-14 11:13:34 +0530

    Changed tests.py file

--- tests.py
+++ tests.py
@@ -1,8 +1,9 @@
 import unittest
 from main import to_upper
+
 class MyTestCase(unittest.TestCase):
     def test_to_upper(self):
-        name = "ADITYA"
+        name = "Aditya"
         upper_name = to_upper(name)
         self.assertEqual(upper_name,"Aditya")
 
