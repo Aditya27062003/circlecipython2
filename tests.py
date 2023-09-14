ed351b85b1d9a23b60aeaaa93e57de959c4322ea
aditya <adityasingh666.model@gmail.com>
2023-09-14 11:00:10 +0530

    Changed config file code 2

--- tests.py
+++ tests.py
@@ -4,7 +4,7 @@ class MyTestCase(unittest.TestCase):
     def test_to_upper(self):
         name = "Aditya"
         upper_name = to_upper(name)
-        self.assertEqual(upper_name, "Aditya")
+        self.assertEqual(upper_name,"Aditya")
 
 
 if __name__ == "__main__":
